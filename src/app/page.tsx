6114e896fc4819291a8336253a524e14db038185
muasya-victor <vicmwe184@gmail.com>
2025-10-31 21:17:44 +0300

    add button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Services from "@/components/home/services/services-component";
 import WebDesign from "@/components/home/web/web-component";
 import { WorkSection } from "@/components/home/work-sectiom";
 import { Menu } from "@/components/layout/menu";
+import { ArrowBigLeft, ArrowRight } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
@@ -16,6 +17,14 @@ export default function Home() {
       <MiniBrand />
       <LogoComponent />
       <WebDesign />
+      <div className="flex flex-col gap-4 p-4 border-b border-gray-200">
+        <h3 className="font-normal leading-snug font-sans text-3xl md:text-5xl">
+          Connect with us to explore your brand's potential
+        </h3>
+        <button className="rounded-full border-3 h-16 w-16 border-black text-black -rotate-45 hover:text-blue-400 hover:border-blue-400 delay-75 hover:zoom-in-5 cursor-pointer">
+          <ArrowRight className="h-12 w-12 " />
+        </button>
+      </div>
     </div>
   );
 }
